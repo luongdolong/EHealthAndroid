a2cbf5dd59d90707cc8a17d3be114e0a7f7aab6a
Luong Do Long <luongdolong@gmail.com>
2017-10-15 01:58:35 +0700

    UP

--- KHospital/app/src/main/java/vn/gov/ehealth/dkonline/ehealth/activity/WebActivity.java
+++ KHospital/app/src/main/java/vn/gov/ehealth/dkonline/ehealth/activity/WebActivity.java
@@ -2,17 +2,20 @@ package vn.gov.ehealth.dkonline.ehealth.activity;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
-import android.content.Intent;
+import android.content.DialogInterface;
 import android.graphics.Bitmap;
+import android.net.http.SslError;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
-import android.webkit.WebChromeClient;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebResourceError;
 import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
-
 import vn.gov.ehealth.dkonline.ehealth.R;
 import vn.gov.ehealth.dkonline.ehealth.common.CommonUtils;
 import vn.gov.ehealth.dkonline.ehealth.common.Constant;
@@ -24,7 +27,9 @@ import vn.gov.ehealth.dkonline.ehealth.common.Constant;
 public class WebActivity extends Activity {
     private Button btWebBack;
     private WebView wvBrowser;
+    private Button btReload;
     private ProgressDialog progDailog;
+    private String mainUrl;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -32,9 +37,10 @@ public class WebActivity extends Activity {
         setContentView(R.layout.activity_web);
         btWebBack = (Button) findViewById(R.id.btWebBack);
         wvBrowser = (WebView) findViewById(R.id.wvBrowser);
-        String url = getIntent().getStringExtra(Constant.EXTRA_URL);
-        if (CommonUtils.isEmpty(url)) {
-            url = getString(R.string.url_main);
+        btReload = (Button) findViewById(R.id.btReload);
+        mainUrl = getIntent().getStringExtra(Constant.EXTRA_URL);
+        if (CommonUtils.isEmpty(mainUrl)) {
+            mainUrl = getString(R.string.url_main);
         }
         btWebBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,8 +50,16 @@ public class WebActivity extends Activity {
                 }
             }
         });
+        btReload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btReload.setVisibility(View.GONE);
+                //wvBrowser.setVisibility(View.VISIBLE);
+                wvBrowser.reload();
+            }
+        });
 
-        progDailog = ProgressDialog.show(this, "Loading","Please wait...", true);
+        progDailog = ProgressDialog.show(this, "Đang tải","Vui lòng chờ đợi...", true);
         progDailog.setCancelable(false);
 
         wvBrowser.getSettings().setJavaScriptEnabled(true);
@@ -56,19 +70,88 @@ public class WebActivity extends Activity {
 
             @Override
             public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                if (!progDailog.isShowing()) {
+                    progDailog.show();
+                }
                 super.onPageStarted(view, url, favicon);
             }
 
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-                progDailog.show();
+                if (!progDailog.isShowing()) {
+                    progDailog.show();
+                }
                 return super.shouldOverrideUrlLoading(view, request);
             }
+
             @Override
             public void onPageFinished(WebView view, final String url) {
+                wvBrowser.setVisibility(View.VISIBLE);
                 progDailog.dismiss();
+                if (!isPageError(view.getTitle())) {
+                    btReload.setVisibility(View.GONE);
+                } else {
+                    wvBrowser.setVisibility(View.INVISIBLE);
+                    showMsg(getString(R.string.error001));
+                    btReload.setVisibility(View.VISIBLE);
+                }
+                super.onPageFinished(view, url);
+            }
+
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                wvBrowser.setVisibility(View.INVISIBLE);
+                progDailog.dismiss();
+                showMsg(getString(R.string.error001));
+                btReload.setVisibility(View.VISIBLE);
+                super.onReceivedError(view, request, error);
+            }
+
+            @Override
+            public void onReceivedHttpError(WebView view, WebResourceRequest request, WebResourceResponse errorResponse) {
+                wvBrowser.setVisibility(View.INVISIBLE);
+                progDailog.dismiss();
+                showMsg(getString(R.string.error001));
+                btReload.setVisibility(View.VISIBLE);
+                super.onReceivedHttpError(view, request, errorResponse);
+            }
+
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                wvBrowser.setVisibility(View.INVISIBLE);
+                progDailog.dismiss();
+                showMsg(getString(R.string.error001));
+                btReload.setVisibility(View.VISIBLE);
+                super.onReceivedSslError(view, handler, error);
             }
         });
-        wvBrowser.loadUrl(url);
+        wvBrowser.loadUrl(mainUrl);
+    }
+
+    private void showMsg(String msg) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(msg);
+        builder.setTitle("Lỗi");
+        builder.setCancelable(true);
+
+        builder.setPositiveButton(
+                "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    private boolean isPageError(String title) {
+        if (title.contains(getString(R.string.domain))) {
+            return false;
+        }
+        if (title.contains("Identification Management System")) {
+            return false;
+        }
+        return true;
     }
 }
